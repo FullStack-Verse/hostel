e287c4207492ca9ddfd30e9281017c46088a1033
MainakRepositor <sultankhilji001@gmail.com>
2023-05-24 19:35:41 +0530

    added files

---
+++ SQL File/hostel.sql
@@ -0,0 +1,382 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 24, 2023 at 03:59 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hostel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updation_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `reg_date`, `updation_date`) VALUES
+(1, 'admin', 'anuj.lpu1@gmail.com', 'Test@1234', '2016-04-04 20:31:45', '2016-04-17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlog`
+--
+
+CREATE TABLE `adminlog` (
+  `id` int(11) NOT NULL,
+  `adminid` int(11) NOT NULL,
+  `ip` varbinary(16) NOT NULL,
+  `logintime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `course_code` varchar(255) DEFAULT NULL,
+  `course_sn` varchar(255) DEFAULT NULL,
+  `course_fn` varchar(255) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course_code`, `course_sn`, `course_fn`, `posting_date`) VALUES
+(1, 'B10992', 'B.Tech', 'Bachelor  of Technology', '2020-07-04 19:31:42'),
+(2, 'BCOM1453', 'B.Com', 'Bachelor Of commerce ', '2020-07-04 19:31:42'),
+(3, 'BSC12', 'BSC', 'Bachelor  of Science', '2020-07-04 19:31:42'),
+(4, 'BC36356', 'BCA', 'Bachelor Of Computer Application', '2020-07-04 19:31:42'),
+(5, 'MCA565', 'MCA', 'Master of Computer Application', '2020-07-04 19:31:42'),
+(6, 'MBA75', 'MBA', 'Master of Business Administration', '2020-07-04 19:31:42'),
+(7, 'BE765', 'BE', 'Bachelor of Engineering', '2020-07-04 19:31:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `roomno` int(11) DEFAULT NULL,
+  `seater` int(11) DEFAULT NULL,
+  `feespm` int(11) DEFAULT NULL,
+  `foodstatus` int(11) DEFAULT NULL,
+  `stayfrom` date DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `course` varchar(500) DEFAULT NULL,
+  `regno` int(11) DEFAULT NULL,
+  `firstName` varchar(500) DEFAULT NULL,
+  `middleName` varchar(500) DEFAULT NULL,
+  `lastName` varchar(500) DEFAULT NULL,
+  `gender` varchar(250) DEFAULT NULL,
+  `contactno` bigint(11) DEFAULT NULL,
+  `emailid` varchar(500) DEFAULT NULL,
+  `egycontactno` bigint(11) DEFAULT NULL,
+  `guardianName` varchar(500) DEFAULT NULL,
+  `guardianRelation` varchar(500) DEFAULT NULL,
+  `guardianContactno` bigint(11) DEFAULT NULL,
+  `corresAddress` varchar(500) DEFAULT NULL,
+  `corresCIty` varchar(500) DEFAULT NULL,
+  `corresState` varchar(500) DEFAULT NULL,
+  `corresPincode` int(11) DEFAULT NULL,
+  `pmntAddress` varchar(500) DEFAULT NULL,
+  `pmntCity` varchar(500) DEFAULT NULL,
+  `pmnatetState` varchar(500) DEFAULT NULL,
+  `pmntPincode` int(11) DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`id`, `roomno`, `seater`, `feespm`, `foodstatus`, `stayfrom`, `duration`, `course`, `regno`, `firstName`, `middleName`, `lastName`, `gender`, `contactno`, `emailid`, `egycontactno`, `guardianName`, `guardianRelation`, `guardianContactno`, `corresAddress`, `corresCIty`, `corresState`, `corresPincode`, `pmntAddress`, `pmntCity`, `pmnatetState`, `pmntPincode`, `postingDate`, `updationDate`) VALUES
+(3, 102, 2, 35000, 1, '2023-05-24', 6, 'Bachelor  of Technology', 0, 'MAINAK', '', 'CHAUDHURI', 'male', 9876543210, 'mainak@gmail.com', 7894561230, 'LUCIFER CHAUDHURI', 'SELF', 7896541230, '1/1 Bablagachi Street, Kolkata - 700002', 'Kolkata', 'West Bengal', 712311, '1/1 Bablagachi Street, Kolkata - 700002', 'Kolkata', 'West Bengal', 712311, '2023-05-24 13:58:50', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `seater` int(11) DEFAULT NULL,
+  `room_no` int(11) DEFAULT NULL,
+  `fees` int(11) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `seater`, `room_no`, `fees`, `posting_date`) VALUES
+(6, 2, 102, 35000, '2023-05-24 13:48:33'),
+(7, 2, 103, 35000, '2023-05-24 13:49:45'),
+(8, 2, 104, 35000, '2023-05-24 13:49:56'),
+(9, 2, 105, 35000, '2023-05-24 13:50:06'),
+(10, 3, 106, 25000, '2023-05-24 13:50:20'),
+(11, 3, 107, 25000, '2023-05-24 13:50:30'),
+(12, 3, 108, 25000, '2023-05-24 13:50:45'),
+(13, 3, 109, 25000, '2023-05-24 13:50:57'),
+(14, 3, 110, 25000, '2023-05-24 13:51:10'),
+(15, 4, 111, 20000, '2023-05-24 13:51:24'),
+(16, 4, 112, 20000, '2023-05-24 13:51:34'),
+(17, 4, 113, 20000, '2023-05-24 13:51:49'),
+(18, 4, 114, 20000, '2023-05-24 13:52:01'),
+(19, 5, 115, 15000, '2023-05-24 13:52:10'),
+(20, 1, 201, 40000, '2023-05-24 13:52:34'),
+(21, 2, 202, 35000, '2023-05-24 13:53:21'),
+(22, 2, 203, 35000, '2023-05-24 13:53:59'),
+(23, 2, 204, 35000, '2023-05-24 13:54:11'),
+(24, 2, 205, 35000, '2023-05-24 13:54:18'),
+(25, 3, 206, 25000, '2023-05-24 13:54:29'),
+(26, 3, 207, 25000, '2023-05-24 13:54:41'),
+(27, 3, 208, 25000, '2023-05-24 13:54:54'),
+(28, 3, 209, 25000, '2023-05-24 13:55:05'),
+(29, 3, 210, 25000, '2023-05-24 13:55:15'),
+(30, 4, 211, 20000, '2023-05-24 13:55:28'),
+(31, 4, 212, 20000, '2023-05-24 13:55:41'),
+(32, 4, 213, 20000, '2023-05-24 13:55:53'),
+(33, 4, 214, 20000, '2023-05-24 13:56:06'),
+(34, 5, 215, 15000, '2023-05-24 13:56:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `State` varchar(150) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `State`) VALUES
+(1, 'Andaman and Nicobar Island (UT)'),
+(2, 'Andhra Pradesh'),
+(3, 'Arunachal Pradesh'),
+(4, 'Assam'),
+(5, 'Bihar'),
+(6, 'Chandigarh (UT)'),
+(7, 'Chhattisgarh'),
+(8, 'Dadra and Nagar Haveli (UT)'),
+(9, 'Daman and Diu (UT)'),
+(10, 'Delhi (NCT)'),
+(11, 'Goa'),
+(12, 'Gujarat'),
+(13, 'Haryana'),
+(14, 'Himachal Pradesh'),
+(15, 'Jammu and Kashmir'),
+(16, 'Jharkhand'),
+(17, 'Karnataka'),
+(18, 'Kerala'),
+(19, 'Lakshadweep (UT)'),
+(20, 'Madhya Pradesh'),
+(21, 'Maharashtra'),
+(22, 'Manipur'),
+(23, 'Meghalaya'),
+(24, 'Mizoram'),
+(25, 'Nagaland'),
+(26, 'Odisha'),
+(27, 'Puducherry (UT)'),
+(28, 'Punjab'),
+(29, 'Rajastha'),
+(30, 'Sikkim'),
+(31, 'Tamil Nadu'),
+(32, 'Telangana'),
+(33, 'Tripura'),
+(34, 'Uttarakhand'),
+(35, 'Uttar Pradesh'),
+(36, 'West Bengal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  `userIp` varbinary(16) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `loginTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `userId`, `userEmail`, `userIp`, `city`, `country`, `loginTime`) VALUES
+(6, 3, '10806121', 0x3a3a31, '', '', '2020-07-20 14:56:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userregistration`
+--
+
+CREATE TABLE `userregistration` (
+  `id` int(11) NOT NULL,
+  `regNo` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `middleName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `contactNo` bigint(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `regDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(45) DEFAULT NULL,
+  `passUdateDate` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `userregistration`
+--
+
+INSERT INTO `userregistration` (`id`, `regNo`, `firstName`, `middleName`, `lastName`, `gender`, `contactNo`, `email`, `password`, `regDate`, `updationDate`, `passUdateDate`) VALUES
+(3, '10806121', 'Anuj', '', 'kumar', 'male', 1234567890, 'test@gmail.com', 'Test@123', '2020-07-20 14:56:18', NULL, NULL),
+(4, 'RA1911027010039', 'MAINAK', '', 'CHAUDHURI', 'male', 9876543210, 'mainak@gmail.com', '9876543210', '2023-05-24 13:58:50', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `room_no` (`room_no`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userregistration`
+--
+ALTER TABLE `userregistration`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `userregistration`
+--
+ALTER TABLE `userregistration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
